a8142fc59ae038ade435c64350d090e98165c06c
Aaron Scribner <aaron.scribner@gmfinancial.com>
2022-09-22 15:23:26 -0400

    LoanAppSubmitted event testing

--- pub_sub/csharp/http/checkout/Program.cs
+++ pub_sub/csharp/http/checkout/Program.cs
@@ -3,8 +3,8 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 var baseURL = (Environment.GetEnvironmentVariable("BASE_URL") ?? "http://localhost") + ":" + (Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "3500"); //reconfigure cpde to make requests to Dapr sidecar
-const string PUBSUBNAME = "orderpubsub";
-const string TOPIC = "orders";
+const string PUBSUBNAME = "applications";
+const string TOPIC = "submissions";
 Console.WriteLine($"Publishing to baseURL: {baseURL}, Pubsub Name: {PUBSUBNAME}, Topic: {TOPIC} ");
 
 var httpClient = new HttpClient();
@@ -23,3 +23,73 @@ for (int i = 1; i <= 10; i++) {
 }
 
 public record Order([property: JsonPropertyName("orderId")] int OrderId);
+
+// var loanApplication = new LoanApplication
+// {
+//     Id = "632b430f435eb3c03b7a804e",
+//     Applicants = new List<Applicant>()
+//     {
+//         new Applicant
+//         {
+//             UserId = "aaron.scribner@gmf.com",
+//             FirstName = "Aaron",
+//             LastName = "Scribner"
+//         }
+//     },
+// };
+//
+// var loanApplicationEvent = new LoanApplicationSubmittedEvent(loanApplication);
+//
+// var loanApplicationEventJson = JsonSerializer.Serialize((loanApplicationEvent));
+// var content = new StringContent(loanApplicationEventJson, Encoding.UTF8, "application/json");
+// var response = httpClient.PostAsync($"{baseURL}/v1.0/publish/{PUBSUBNAME}/{TOPIC}", content);
+// Console.WriteLine("Published data: " + content);
+
+
+public class LoanApplicationSubmittedEvent 
+{
+    public LoanApplicationSubmittedEvent(LoanApplication loanApplication)
+    {
+        LoanApplication = loanApplication;
+    }
+    
+    public LoanApplication LoanApplication { get; }
+}
+
+public class LoanApplication
+{
+    [property: JsonPropertyName("id")] 
+    public string? Id { get; set; }
+
+    [property: JsonPropertyName("applicants")] 
+    public List<Applicant> Applicants { get; set; } = new();
+}
+
+public enum LoanApplicationStatus
+{
+    New,
+    Submitted,
+    Approved,
+    Declined,
+    Pending,
+    Underwriting,
+    Abandoned
+}
+
+public class Applicant
+{
+    [property: JsonPropertyName("id")] 
+    public string? Id { get; set; }
+    
+    [property: JsonPropertyName("userId")] 
+    public string UserId { get; set; }
+    
+    [property: JsonPropertyName("firstName")] 
+    public string FirstName { get; set; }
+    
+    [property: JsonPropertyName("lastName")] 
+    public string LastName { get; set; }
+    
+    [property: JsonPropertyName("middleName")] 
+    public string? MiddleName { get; set; }
+}
--- pub_sub/csharp/http/order-processor/Program.cs
+++ pub_sub/csharp/http/order-processor/Program.cs
@@ -8,13 +8,13 @@ if (app.Environment.IsDevelopment()) {app.UseDeveloperExceptionPage();}
 
 // Register Dapr pub/sub subscriptions
 app.MapGet("/dapr/subscribe", () => {
-    var sub = new DaprSubscription(PubsubName: "orderpubsub", Topic: "orders", Route: "orders");
+    var sub = new DaprSubscription(PubsubName: "applications", Topic: "submissions", Route: "submissions");
     Console.WriteLine("Dapr pub/sub is subscribed to: " + sub);
     return Results.Json(new DaprSubscription[]{sub});
 });
 
 // Dapr subscription in /dapr/subscribe sets up this route
-app.MapPost("/orders", (DaprData<Order> requestData) => {
+app.MapPost("/submissions", (DaprData<Order> requestData) => {
     Console.WriteLine("Subscriber received : " + requestData.Data.OrderId);
     return Results.Ok(requestData.Data);
 });
@@ -27,3 +27,51 @@ public record DaprSubscription(
   [property: JsonPropertyName("pubsubname")] string PubsubName, 
   [property: JsonPropertyName("topic")] string Topic, 
   [property: JsonPropertyName("route")] string Route);
+
+public class LoanApplicationSubmittedEvent 
+{
+    public LoanApplicationSubmittedEvent(LoanApplication loanApplication)
+    {
+        LoanApplication = loanApplication;
+    }
+    
+    public LoanApplication LoanApplication { get; }
+}
+
+public class LoanApplication
+{
+    [property: JsonPropertyName("id")] 
+    public string? Id { get; set; }
+
+    [property: JsonPropertyName("applicants")] 
+    public List<Applicant> Applicants { get; set; } = new();
+}
+
+public enum LoanApplicationStatus
+{
+    New,
+    Submitted,
+    Approved,
+    Declined,
+    Pending,
+    Underwriting,
+    Abandoned
+}
+
+public class Applicant
+{
+    [property: JsonPropertyName("id")] 
+    public string? Id { get; set; }
+    
+    [property: JsonPropertyName("userId")] 
+    public string UserId { get; set; }
+    
+    [property: JsonPropertyName("firstName")] 
+    public string FirstName { get; set; }
+    
+    [property: JsonPropertyName("lastName")] 
+    public string LastName { get; set; }
+    
+    [property: JsonPropertyName("middleName")] 
+    public string? MiddleName { get; set; }
+}
